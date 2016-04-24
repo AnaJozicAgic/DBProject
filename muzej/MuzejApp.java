7b65ed7824ee0e2ba0dbadc107326a4ae5c4296b
Ana Jozic Agic <ana.jozic.agic@kolektiv.eu>
2016-04-24 23:09:07 +0200

    Izmjenje u aktivnostima korisnika i administratora.

--- muzej/MuzejApp.java
+++ muzej/MuzejApp.java
@@ -5,8 +5,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class MuzejApp {
+	public static Scanner input = new Scanner(System.in);
+
 	public static boolean checkDBExists(String dbName) {
 
 		try {
@@ -53,20 +56,131 @@ public class MuzejApp {
 		}
 	}
 
-	public static void admin(String[] args) {
+	public static void admin() {
 		// CRUD
-		// Create
-		// Update
-		// Reed
-		// Delete
 
+		MuzejDAOConcrete admin = new MuzejDAOConcrete();
+		System.out.println(
+				"Unesite 1 - za azuriranje/dodavanje novog exponata. \nUnesite 2 - za iscitavanje/pretrazivanje exponata. \nUnesite 3 - za brisanje exponata.\nUnesite 4 - za logout.");
+		int check1 = input.nextInt();
+
+		switch (check1) {
+		case 1:
+			System.out.println("Unesite 1 - za azuriranje exponata\nUnesite 2 - za dodavanje exponata");
+			int izbor = input.nextInt();
+			switch (izbor) {
+			case 1:
+				try {
+					System.out.println("Unesite id exponata koji zelite azurirati : ");
+					int id = input.nextInt();
+					admin.updateMuzejDB(admin.getMuzejDB(id));
+					System.out.println();
+				} catch (SQLException e) {
+					System.out.println("_");
+				}
+				break;
+			case 2:
+				try {
+					admin.addMuzejDB();
+				} catch (SQLException e) {
+					System.out.println("+");
+				}
+				break;
+			default:
+				break;
+			}
+
+			break;
+		case 2:
+			System.out.println("Unesite 1 - za izlistavanje svih exponata \nUnesite 2 - za pretrazivanje exponata. ");
+			int list_serch = input.nextInt();
+			switch (list_serch) {
+			case 1:
+				try {
+					admin.getMuzejDB();
+				} catch (SQLException e) {
+					System.out.println("-");
+				}
+				break;
+			case 2:
+
+				System.out.println(
+						"1 unesite za pretrazivanje po id-u,\n2 unesite za pretrazivanje po nazivu \n3 unesite za pretrazivanje po lokalitetu \n4 unesite za pretrazivanje po procjenjenoj starosti. ");
+				int provjera = input.nextInt();
+				switch (provjera) {
+				case 1:
+					System.out.println("Unesite id zeljenog exponata: ");
+					int id = input.nextInt();
+					try {
+						System.out.println(admin.getMuzejDB(id));
+
+					} catch (SQLException e1) {
+
+					}
+
+					break;
+				case 2:
+					System.out.println("Unesite naziv zeljenog exponata: ");
+					String naziv1 = input.next();
+
+					try {
+						System.out.println(admin.getMuzejDBname(naziv1));
+					} catch (SQLException e) {
+						System.out.println("");
+					}
+					break;
+				case 3:
+					System.out.println("Unesite lokalitet :");
+					String lokalitet = input.next();
+					try {
+						System.out.println(admin.getMuzejDBplace(lokalitet));
+					} catch (SQLException e) {
+
+					}
+					break;
+				case 4:
+					System.out.println("Unesite procjenjenu starost : ");
+					int starost = input.nextInt();
+					try {
+						System.out.println(admin.getMuzejDBage(starost));
+					} catch (SQLException e) {
+						System.out.println("-");
+					}
+				break;
+					
+			default:
+				break;
+			}
+			//point
+			break;
+		case 3:
+			System.out.println("Unesite id exponata koji zelite ukloniti :");
+			
+			try {
+				int id = input.nextInt();
+				admin.deleteMuzejDB(admin.getMuzejDB(id));
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (Exception e) {
+				System.out.println("Pogresan unos. ");
+			}
+			break;
+		case 4:
+		login("", "");
+			break;
+			
+		default:
+			break;
+		}
+		}//point2
 	}
 
-	public static void user(String[] args) {
+	public static void user() {
 		// izlistati bazu
 
 		// Pretrazivati bazu
-		java.util.Scanner input = new java.util.Scanner(System.in);
+
 		MuzejDAOConcrete user = new MuzejDAOConcrete();
 		System.out.println(
 				"1 - Unesite za izlistavanje cijele baze.\n2 - Unesite za pretrazivanje baze. \n3 - Unesite za logout.");
@@ -129,24 +243,27 @@ public class MuzejApp {
 			}
 			break;
 		case 3:
-			try {
-				main(args);
-			} catch (SQLException e2) {
-				System.out.println("Greska.");
-			}
+			login("", "");
+
 			break;
 		default:
 			break;
 		}
 
+		try {
+			user.close();
+		} catch (Exception e) {
+
+		}
+
 	}
 
-	public static boolean login(String username, String pasword, String[] args) {
+	public static boolean login(String username, String pasword) {
 		if (Users.getUser(username, pasword).equals("administrator")) {
-			admin(args);
+			admin();
 			return true;
 		} else if (Users.getUser(username, pasword).equals("user")) {
-			user(args);
+			user();
 			return true;
 		} else {
 
@@ -160,19 +277,19 @@ public class MuzejApp {
 			create();
 		}
 		String username = "", pasword = "";
-		java.util.Scanner input = new java.util.Scanner(System.in);
-		while (!login(username, pasword, args)) {
+
+		while (!login(username, pasword)) {
 
 			System.out.println("Enter username: ");
 			username = input.next();
 			System.out.println("Enter password: ");
 			pasword = input.next();
-			if (!login(username, pasword, args)) {
+			if (!login(username, pasword)) {
 
 				System.out.println("You entered incorect walues. ");
 			}
 		}
-		input.close();
+		// input.close();
 	}
 
 }
